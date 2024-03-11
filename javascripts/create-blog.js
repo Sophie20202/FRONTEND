6f301ceb41bfa60c99fa8a2da70076b20793d9a1
Sophie Mukamugema <sofidele12@.com>
2024-03-11 03:13:28 -0700

    reterrtt

--- javascripts/create-blog.js
+++ javascripts/create-blog.js
@@ -2,6 +2,7 @@ window.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("articleForm");
   form.addEventListener("submit", function (e) {
     e.preventDefault();
+    let blogImage =  processImage(fileInput);
     var title = document.getElementById("title").value;
     var articleContent = document.getElementById("articleContent").value;
     var fileInput = document.getElementById("picture");
@@ -10,6 +11,7 @@ window.addEventListener("DOMContentLoaded", function () {
     create.append("title", title);
     create.append("message", articleContent);
     create.append("image", fileInput.files[0]);
+    create.append("blogImage",blogImage )
 
     fetch("https://backend-jdw6.onrender.com/greet/v1/blog", {
       method: "POST",
@@ -29,4 +31,18 @@ window.addEventListener("DOMContentLoaded", function () {
         window.location.href = "adminpanel.html";
       });
   });
+  const processImage = (input) => {
+    return new Promise((resolve, reject) => {
+        if (input.files && input.files[0]) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                resolve(e.target.result);
+            };
+            reader.readAsDataURL(input.files[0]);
+        } else {
+            reject("No file selected");
+        }
+    });
+};
+  
 });
